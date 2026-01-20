fb4321a97414d47f82e0b0b4478546081b2773ad
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-01-20 22:52:57 +0530

    messages and converstation models defined

---
+++ backend/src/models/conversation-model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const User = require('../models/user-model');
+const Message = require('../models/message-model');
+
+const conversationSchema = new mongoose.Schema({
+ participants:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: User,
+ }] ,
+ messsages: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref : Message,
+    default:[]
+ }] 
+} , 
+{timestamps:true}
+)
+
+module.exports = mongoose.model("Conversation", conversationSchema);
